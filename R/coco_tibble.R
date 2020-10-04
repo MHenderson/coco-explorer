5cd66536a69dc21bd14d715102ceed53bf47aa2f
Matthew Henderson <matthew.james.henderson@gmail.com>
2020-10-04 11:13:21 +0100

    move to the coco-data repo

--- R/coco_tibble.R
+++
@@ -1,9 +0,0 @@
-coco_tibble <- function(works, left, right, fdr, span) {
-  CorporaCoCo::surface_coco(
-    a = works[[left]],
-    b = works[[right]],
-    fdr = fdr,
-    span = span,
-    nodes = c('back', 'eye', 'eyes', 'forehead', 'hand', 'hands', 'head', 'shoulder')
-  )
-}
